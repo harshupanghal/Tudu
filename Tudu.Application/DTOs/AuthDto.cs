c15a5cdff5dce84566e845e6ea4d92856abbaaf7
harshupanghal <harshu7850@gmail.com>
2025-08-09 13:14:15 +0530

    ui updated

--- Tudu.Application/DTOs/AuthDto.cs
+++ Tudu.Application/DTOs/AuthDto.cs
@@ -2,10 +2,17 @@
 
 namespace Tudu.Application.DTOs;
 public class RegisterRequest
+    
     {
-    [Required(ErrorMessage = "Username is required")]
+    [Required(ErrorMessage = "Username is required.")]
+    [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 20 characters.")]
+    [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Username can only contain letters, numbers, and underscores.")]
     public string UserName { get; set; } = string.Empty;
-    [Required(ErrorMessage = "Password is required")]
+
+    [Required(ErrorMessage = "Password is required.")]
+    [StringLength(50, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+    [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$",
+        ErrorMessage = "Password must contain at least 1 uppercase, 1 lowercase, and 1 digit.")]
     public string Password { get; set; } = string.Empty;
     }
 
